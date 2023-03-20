803c0db43055645d3df4dbd874356a1b4b29a775
Miks_Zeltins_221RDB123 <97308677+MiksZelt@users.noreply.github.com>
2023-03-20 15:39:37 +0000

    221RDB123

--- build_heap.py
+++ build_heap.py
@@ -10,10 +10,10 @@ def build_heap(data):
     return swaps
 
 def sift(amount, data, swaps, i):
-    min_index = i
     left_child = 2*i+1
     right_child = 2*i+2
-    
+    min_index = i
+
     if left_child < amount and data[min_index] > data[left_child]:
         min_index = left_child
     if right_child < amount and data[min_index] > data[right_child]:
@@ -29,9 +29,8 @@ def main():
     text = input()
     if text == "I":
         n = int(input())
-        data = list(map(int, input().strip().split()))
-
-        assert len(data) == n
+        data = list((map(int, input().strip().split())))
+        assert len(data) == n , "Neprecizs elementu daudzums"
         swaps = build_heap(data)
         print(len(swaps))
         for i, j in swaps:
@@ -44,11 +43,9 @@ def main():
             file_path = "tests/" + file_name
             with open(file_path, "r", encoding="utf-8") as f:
                 n = int(f.readline().strip())
-                data = list(map(int, f.readline().strip().split()))
+                data = list((map(int, f.readline().strip().split())))
                 swaps = build_heap(data)
-
-            assert len(data) == n
-
+            assert len(data) == n , "Neprecizs elementu daudzums"
             print(len(swaps))
             for i, j in swaps:
                 print(i, j)
