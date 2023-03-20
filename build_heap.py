65eb91766fad507975c8d62b0c8861d70b3d6c36
Miks_Zeltins_221RDB123 <97308677+MiksZelt@users.noreply.github.com>
2023-03-20 14:51:23 +0000

    update9

--- build_heap.py
+++ build_heap.py
@@ -35,11 +35,13 @@ def main():
         print(len(swaps))
         for i, j in swaps:
             print(i, j)
+
+            
     elif "F" in text:
         file_name = input()
         if "a" not in file_name:
             file_path = "tests/" + file_name
-            with open(fails,"r", encoding="utf-8") as f:
+            with open(dile_path, "r", encoding="utf-8") as f:
                 n = int(f.readline().strip())
                 data = list((map(int, f.readline().strip().split())))
                 swaps = build_heap(data)
